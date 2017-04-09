2dd4bd1a5a6473d9a4ab1b1faa7ddcf732a5c7ad
Joakim Carlsten <joakim@vinnia.se>
2017-04-09 18:07:44 +0200

    Handle exception thrown when wrong mailchimp API key given.

---
+++ README.md
@@ -0,0 +1,6 @@
+#Newsletter plugin
+
+## Template overloading
+Should be put in 
+ - /views/newsletter/signupForm.php
+ - /views/newsletter/modal.php
--- src/Admin/SettingsPage.php
+++ src/Admin/SettingsPage.php
@@ -6,6 +6,7 @@
  * Date: 19/09/16
  * Time: 10:35
  */
+
 namespace NewsletterSignup\Admin;
 
 use NewsletterSignup\Vendor\MailChimp\MailChimp;
@@ -20,9 +21,14 @@ class SettingsPage extends AbstractSettingsPage
         $settings_data = $this->get_settings_data();
 
         $lists = [];
+        $error = '';
         if (!empty($settings_data['mailchimp_api_key'])) {
-            $MailChimp = new MailChimp($settings_data['mailchimp_api_key']);
-            $lists = $MailChimp->get('lists');
+            try {
+                $MailChimp = new MailChimp($settings_data['mailchimp_api_key']);
+                $lists = $MailChimp->get('lists');
+            } catch (\Exception $e) {
+                $error = 'Invalid Mailchimp API key provided';
+            }
         }
 
         ?>
@@ -30,11 +36,18 @@ class SettingsPage extends AbstractSettingsPage
             <form method="post" action="options.php">
                 <?php
                 settings_fields($option_group);
+
+                if (!empty($error)) :
+                    ?>
+                    <div class="alert"><?= $error; ?></div>
+                    <?php
+                endif;
                 ?>
                 <h2>Mailchimp</h2>
 
                 <label for="mailchimp_api_key">Mailchimp API key</label>
-                <input type="text" id="mailchimp_api_key" name="<?php echo esc_attr($option_name . "[mailchimp_api_key]"); ?>"
+                <input type="text" id="mailchimp_api_key"
+                       name="<?php echo esc_attr($option_name . "[mailchimp_api_key]"); ?>"
                        value="<?php echo esc_attr($settings_data['mailchimp_api_key']); ?>"/>
 
 
